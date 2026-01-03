8b7d40105efc5501d267f145e3f49faff53ab659
Gasanova Tamara <solas93@mail.ru>
2026-01-03 17:56:16 +0300

    feat: implement add_task

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "./headers/todo.h"
 #include "./headers/storage.h"
+#include "./headers/todo_types.h"
 
 int main() {
+    TodoList list = {};
+    add_task(&list, "Create add task function");
     printf("TODO application \n");
+    printf("%p \n", &list);
+    printf("%s \nDone: %s \n", list.todos[0].task, list.todos[0].done ? "[x]" : "[ ]");
     return 0;
 }
--- todo.c
+++ todo.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include "./headers/todo_types.h"
 #include "./headers/todo.h"
 
 void add_task(TodoList *list, const char *task) {
-    // To implement
+    if (list->count >= MAX_TODOS_H) {
+        printf("Cannot add more tasks: \n you can add up to 100 tasks :)");
+    }
+    
+    Todo *t = &list->todos[list->count]; // get memory slot
+    t->id = list->count; // add id
+    strncpy(t->task, task, LEN_TASK-1); // copy the new todo
+    t->task[LEN_TASK-1] = '\0'; // to avoid null-terminated string
+    t->done = false; // is the todo done
+
+    list->count++;
 }
 
 void list_tasks(TodoList *list) {
