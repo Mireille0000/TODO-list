e4bf557263abac88f782b5da493d2c5c3c887ab6
Gasanova Tamara <solas93@mail.ru>
2026-01-11 02:47:44 +0300

    feat: implement save_todo_list

--- main.c
+++ main.c
@@ -7,9 +7,8 @@ int main() {
     TodoList list = {0};
     add_task(&list, "Create add task function");
     add_task(&list, "Create list tasks function");
-    add_task(&list, "Create delete task function");
-    delete_task(&list, 1);
     mark_done(&list, 0);
     list_tasks(&list);
+    save_todo_list(&list, "todo.txt");
     return 0;
 }
--- storage.c
+++ storage.c
@@ -3,7 +3,19 @@
 #include "./headers/todo_types.h"
 
 void save_todo_list(const TodoList *list, const char *file){
-    // To implement
+    FILE *todo_list;
+    todo_list = fopen(file, "w");
+    printf("HEY");
+    if(!todo_list) {
+        return;
+    }
+    int i = 0;
+    printf("%d\n", list->count);
+    for(i; i < list->count; i++) {
+        printf("%d\n", list->todos[i].done);
+        fprintf(todo_list, "%s | %s\n", list->todos[i].task, list->todos[i].done ? "[x]" : "[ ]");
+    }
+    fclose(todo_list);
 }
 
 void load_todo_list(TodoList *list, const char *file) {
